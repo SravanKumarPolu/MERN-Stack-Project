6ff87e6311ddd0606b2c89d27fc693a700b4334b
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-12 20:35:53 +0530

    login page created
    whenever click avatar image it take back to login page

--- front-end/src/components/NavBar.tsx
+++ front-end/src/components/NavBar.tsx
@@ -43,7 +43,10 @@ const NavBar: React.FC = () => {
       <div className='flex item-center gap-6'>
         <img onClick={() => setShowSearch(true)} src={search} className="w-6 cursor-pointer " alt="Search" />
         <div className='group relative'>
-          <img src={profile} className="w-6 cursor-pointer " alt='Profile' />
+          <Link to='/login'>
+            <img src={profile} className="w-6 cursor-pointer " alt='Profile' />
+          </Link>
+
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
             <div className='flex flex-col gap-2 w-36 px-5 bg-slate-100 text-gray-500 rounded '>
               <p className='cursor-pointer hover:text-black'>My Profile</p>
