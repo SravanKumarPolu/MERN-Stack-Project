a159f4455bc3d5f8123502dca2b38f7649a10c1f
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-13 21:52:30 +0530

    back end  server js file

---
+++ back-end/server.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+
+import connectDB from './config/mongodb.js';
+//sravanpolume:username, pass:9OjiMDGPpOeln2Vy
+//mongodb+srv://sravanpolume:9OjiMDGPpOeln2Vy@cluster0.zuxiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+import cors from 'cors'
+import express from 'express'
+
+//App config
+const app = express();
+const port = process.env.PORT || 4000
+
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.get('/', (req, res) => {
+  res.send("API Working")
+})
+
+app.listen(port, () => console.log('server started on PORT:' + port))
