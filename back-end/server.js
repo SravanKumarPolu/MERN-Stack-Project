72d736b58b38a94704222156108c49f0087edb01
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-14 00:26:42 +0530

    cloudinary js file created
    the one called inside server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 
+import connectCloudinary from './config/cloudinary.js';
 import connectDB from './config/mongodb.js';
 //sravanpolume:username, pass:9OjiMDGPpOeln2Vy
 //mongodb+srv://sravanpolume:9OjiMDGPpOeln2Vy@cluster0.zuxiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
@@ -10,6 +11,7 @@ import express from 'express'
 const app = express();
 const port = process.env.PORT || 4000
 connectDB()
+connectCloudinary()
 
 //middlewares
 app.use(express.json())
