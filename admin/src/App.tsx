75c34b34d575ee99733f521cc4d9ca3592cd62e4
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-17 00:15:43 +0530

    typescript bug fixed

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,50 +1,50 @@
 import 'react-toastify/dist/ReactToastify.css';
 
-import { Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom"
+import { useEffect, useState } from "react"
 
-import Add from "./pages/Add";
-import List from "./pages/List";
-import Login from "./components/Login";
-import Navbar from "./components/Navbar";
-import Orders from "./pages/Orders";
-import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add"
+import List from "./pages/List"
+import Login from "./components/Login"
+import Navbar from "./components/Navbar"
+import Orders from "./pages/Orders"
+import Sidebar from "./components/Sidebar"
 import { ToastContainer } from 'react-toastify';
 
-export const backendUrl: string = import.meta.env.VITE_BACKED_URL;
-
-const App: React.FC = () => {
+export const backendUrl = import.meta.env.VITE_BACKED_URL
+const App = () => {
   const [token, setToken] = useState<string>(
     localStorage.getItem('token') || ""
   );
 
   useEffect(() => {
-    localStorage.setItem("token", token);
-  }, [token]);
-
+    localStorage.setItem("token", token)
+  }, [token])
   return (
     <div className="bg-gray-50 min-h-screen">
       <ToastContainer />
-      {token === "" ? (
-        <Login setToken={setToken} />
-      ) : (
-        <>
-          <Navbar />
-          <hr />
-          <div className="flex w-full">
-            <Sidebar />
-            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-              <Routes>
-                <Route path="/add" element={<Add />} />
-                <Route path="/list" element={<List />} />
-                <Route path="/orders" element={<Orders />} />
-              </Routes>
+      {
+        token === ""
+          ? <Login setToken={setToken} />
+          : <>
+            <Navbar />
+            <hr />
+            <div className="flex w-full">
+              <Sidebar />
+              <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+                <Routes>
+                  <Route path="/add" element={<Add />} />
+                  <Route path="/list" element={<List />} />
+                  <Route path="/orders" element={<Orders />} />
+                </Routes>
+              </div>
             </div>
-          </div>
-          </>
-      )}
-    </div>
-  );
-};
 
-export default App;
+          </>
+      }
+
+    </div>
+  )
+}
+
+export default App
