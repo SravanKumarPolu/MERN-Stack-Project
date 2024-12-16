e56d861e7a3691956abb468e2532f75cea0d354e
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-16 23:06:07 +0530

    Nabar and side bar

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,8 +1,38 @@
+import { Route, Routes } from "react-router-dom"
 
+import Add from "./pages/Add"
+import List from "./pages/List"
+import Login from "./components/Login"
+import Navbar from "./components/Navbar"
+import Orders from "./pages/Orders"
+import Sidebar from "./components/Sidebar"
+import { useState } from "react"
 
-function App() {
+const App = () => {
+  const [token, setToken] = useState('')
   return (
-    <div>App</div>
+    <div className="bg-gray-50 min-h-screen">
+      {
+        token === ""
+          ? <Login />
+          : <>
+            <Navbar />
+            <hr />
+            <div className="flex w-full">
+              <Sidebar />
+              <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+                <Routes>
+                  <Route path="/add" element={<Add />} />
+                  <Route path="/list" element={<List />} />
+                  <Route path="/orders" element={<Orders />} />
+                </Routes>
+              </div>
+            </div>
+
+          </>
+      }
+
+    </div>
   )
 }
 
