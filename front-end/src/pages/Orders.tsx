d02c52626cc5c98e705060b188a38abf6984c8cf
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-22 21:44:58 +0530

    The User Order Data functionality in the backend is used to display on the frontend whatever is stored in the database order section.

--- front-end/src/pages/Orders.tsx
+++ front-end/src/pages/Orders.tsx
@@ -1,6 +1,8 @@
+import { useContext, useEffect, useState } from "react"
+
 import { ShopContext } from "../context/ShopContext"
 import Title from "../components/Title"
-import { useContext } from "react"
+import axios from "axios"
 
 const Orders = () => {
   const context = useContext(ShopContext)
@@ -8,14 +10,45 @@ const Orders = () => {
   if (!context) {
     return <div>Loading...</div>;
   }
-  const { products, currency } = context;
+  const { backendUrl, token, currency } = context;
+  const [orderData, setOrderData] = useState([])
+  const loadOrderData = async () => {
+    try {
+      if (!token) {
+        return null
+      }
+      const response = await axios.post(backendUrl + '/api/order/userorders', {}, { headers: { token } })
+      if (response.data.success) {
+        let allOrdersItem = []
+        response.data.orders.map((order) => {
+          order.items.map((item) => {
+            item['status'] = order.status
+            item['payment'] = order.payment
+            item['paymentmethod'] = order.paymentMethod
+            item['date'] = order.date
+            allOrdersItem.push(item)
+          })
+        })
+        setOrderData(allOrdersItem.reverse())
+      }
+      console.log(response.data)
+
+    } catch (error) {
+      console.error(error);
+      toast.error("An error occurred. Please try again.");
+
+    }
+  }
+  useEffect(() => {
+    loadOrderData()
+  }, [token])
   return (
     <div className="border-t pt-16">
       <div className="text-2xl">
         <Title text1={'MY  '} text2={'ORDERS'} />
       </div>
       <div >
-        {products.slice(1, 4).map((item, index) => (
+        {orderData.map((item, index) => (
           <div key={index} className="  py-4 border-t text-gray-700 flex flex-col md:flex-row md:items-center
          md:justify-between gap-4">
             {/* Product Details */}
